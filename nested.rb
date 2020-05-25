67eb97beb05bc78cfd4d320719e022d406de4f73
Lauren Frankovich <frankovister@gmail.com>
2020-05-25 18:27:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,15 +81,12 @@ def adding_matz
         :languages => ["C"]
       }
     }
-    programmer_hash[:yukihiro_matsumoto] = [
-#      { known_for: "Ruby" },
- #     { languages: ["LISP", "C"] }
-    ]
-  puts programmer_hash
+    programmer_hash[:yukihiro_matsumoto] = {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+    }
+  programmer_hash
 end
-
-
-
  
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
