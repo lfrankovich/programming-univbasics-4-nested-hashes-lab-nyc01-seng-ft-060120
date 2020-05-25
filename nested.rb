adc1c05dfc1feaef482cc5bb3741e6aa69195041
Lauren Frankovich <frankovister@gmail.com>
2020-05-25 18:33:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -130,6 +130,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-    programmer_hash[:dennis_ritchie][:languages]
-
+    programmer_hash[:dennis_ritchie][:languages][3] = "Assembly"
+    programmer_hash
 end
